83a79e82d9388973690da94a1edfdb81486e6b93
jonakls <asmot54@hotmail.com>
2023-10-25 08:28:51 -0500

    feat(manager): add evaluate radix with dataframe and add radix formats types actions

--- src/manager/radix/RadixFormatManager.py
+++ src/manager/radix/RadixFormatManager.py
@@ -1,19 +1,29 @@
-import util.FormatUtil as FormatUtil
-from manager.radix.Manager import Manager
+import numpy as np
+
+from manager.radix import RadixShortManager, RadixLongManager
+from src.util import FormatUtil
 
 
-class RadixFormatManager(Manager):
+class RadixFormatManager:
 
-    def format_short(self):
-        pass
+    def __init__(self, data_frame):
+        self.data_frame = data_frame
+        self.data_frame['RADICADO_C9'] = np.nan
+        self.data_frame['RADICADO_C10'] = np.nan
+        self.format()
 
-    def format_long(self):
-        pass
+    def format(self):
+        if self.data_frame is None:
+            raise Exception('No se ha cargado ningún archivo o simplemente hubo un error al cargarlo.')
 
-    def format_radix(self, value):
-
-        return FormatUtil.format_string(self, True)
-
-    @staticmethod
-    def extract(value):
-        return FormatUtil.clean_numbers(value)
+        for index, row in self.data_frame.iterrows():
+            value = row['RADICACION']
+            if FormatUtil.is_short(value):
+                self.data_frame.at[index, 'RADICADO_C9'] = RadixShortManager.format_c9(value)
+                self.data_frame.at[index, 'RADICADO_C10'] = RadixShortManager.format_c10(value)
+            elif FormatUtil.is_long(value):
+                self.data_frame.at[index, 'RADICADO_C10'] = RadixLongManager.format_c10(value)
+                self.data_frame.at[index, 'RADICADO_C9'] = RadixLongManager.format_c9(value)
+            else:
+                self.data_frame.at[index, 'RADICADO_C9'] = '0'
+                self.data_frame.at[index, 'RADICADO_C10'] = '0'
