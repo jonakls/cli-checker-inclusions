cfda860783a661a1f9c83759f3c52d6ef6c517a9
jonakls <asmot54@hotmail.com>
2023-11-07 15:58:58 -0500

    feat(serializer): add features with another files format and process more columns

--- src/serialize/CSVSerializer.py
+++ src/serialize/CSVSerializer.py
@@ -1,16 +1,43 @@
 import pandas as pd
 
 
-def process_csv(input_path):
-    return pd.read_csv(input_path, delimiter='\t', encoding='latin1')
+def process_file(input_path):
+    try:
+        with open(input_path, 'r') as file:
+            file.close()
+            if input_path.endswith('.csv') or input_path.endswith('.txt'):
+                return pd.read_csv(input_path, delimiter='\t', encoding='latin1')
+            elif input_path.endswith('.xlsx'):
+                return pd.read_excel(input_path)
+            else:
+                print('Este archivo no tiene un formato válido.')
+                return None
+    except PermissionError:
+        print('No se pudo leer el archivo, puede que esté abierto o otro programa lo esté usando.')
+        return None
 
 
 def deserialize(input_path):
-    data_frame = process_csv(input_path)
+    data_frame = process_file(input_path)
+
+    if data_frame is None:
+        return
+
     data_frame.head()
-    radix_list = data_frame[["PROCESO_ID", "RADICACION"]]
-    return radix_list
+    if 'PROCESO_ID' in data_frame.columns:
+        radix_context = data_frame[['PROCESO_ID', 'RADICACION']]
+    elif 'BASE_ID' in data_frame.columns:
+        radix_context = data_frame[['BASE_ID', 'RADICACION']]
+    else:
+        print('Este archivo no tiene un formato válido.')
+        return None
+    return radix_context
 
 
 def serialize(output_path, data_frame):
-    data_frame.to_csv(output_path, sep=';', index=False)
+    try:
+        with open(output_path, 'w') as file:
+            file.close()
+            data_frame.to_csv(output_path, sep=';', index=False)
+    except PermissionError:
+        return None
