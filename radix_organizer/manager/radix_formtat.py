769edbc014447976ce272358c1aa6c940083fd31
jonakls <asmot54@hotmail.com>
2023-12-07 14:14:58 -0500

    refactor(files): simplify code for apply changes in columns of dataframe

--- radix_organizer/manager/radix_formtat.py
+++ radix_organizer/manager/radix_formtat.py
@@ -4,66 +4,40 @@ import numpy as np
 def init_format(data_frame):
     from radix_organizer.manager import radix_short
     from radix_organizer.manager import radix_long
-    from radix_organizer.util import FormatUtil
+    from radix_organizer.util import format_util
     if data_frame is None:
         raise Exception('No se ha cargado ningún archivo o simplemente hubo un error al cargarlo.')
 
-    data_frame['LARGO_RADICADO'] = np.nan
     data_frame['RADICADO_VIRGEN'] = np.nan
-    data_frame['LARGO_RADICADO_VIRGEN'] = np.nan
     data_frame['RADICADO_C9'] = np.nan
-    data_frame['LARGO_RAD_C9'] = np.nan
     data_frame['RADICADO_C10'] = np.nan
-    data_frame['LARGO_RAD_C10'] = np.nan
 
-    data_frame['RADICADO_VIRGEN'] = data_frame['RADICADO_VIRGEN'].astype('object')
-    data_frame['LARGO_RADICADO_VIRGEN'] = data_frame['LARGO_RADICADO_VIRGEN'].astype('object')
-    data_frame['LARGO_RADICADO'] = data_frame['LARGO_RADICADO'].astype('object')
-    data_frame['RADICADO_C9'] = data_frame['RADICADO_C9'].astype('object')
-    data_frame['RADICADO_C10'] = data_frame['RADICADO_C10'].astype('object')
-    data_frame['LARGO_RAD_C9'] = data_frame['LARGO_RAD_C9'].astype('object')
-    data_frame['LARGO_RAD_C10'] = data_frame['LARGO_RAD_C10'].astype('object')
+    data_frame['RADICADO_VIRGEN'].astype('object')
+    data_frame['RADICADO_C9'].astype('object')
+    data_frame['RADICADO_C10'].astype('object')
 
     if data_frame is None:
         raise Exception('No se ha cargado ningún archivo o simplemente hubo un error al cargarlo.')
 
-    for index, row in data_frame.iterrows():
-        value = row['RADICACION']
+    data_frame['RADICADO_VIRGEN'] = data_frame['RADICACION'].apply(lambda x: virgin_value(x))
 
-        if value is None or value == '':
-            data_frame.at[index, 'LARGO_RADICADO'] = 'Valor nulo'
-            continue
+    data_frame['RADICADO_C9'] = data_frame['RADICACION'].apply(lambda x: (
+        radix_short.format_c9(virgin_value(x, False)) if format_util.is_short(virgin_value(x, False))
+        else radix_long.format_c9(virgin_value(x, False))
+    ))
 
-        if value is None or value == '':
-            data_frame.at[index, 'LARGO_RADICADO'] = 'Valor nulo'
-            continue
-
-        data_frame.at[index, 'LARGO_RADICADO'] = str(value).__len__()
-        clean_value = FormatUtil.clean_numbers(value)
-        data_frame.at[index, 'RADICADO_VIRGEN'] = str("." + clean_value)
-        data_frame.at[index, 'LARGO_RADICADO_VIRGEN'] = str(clean_value).__len__()
-        if FormatUtil.is_short(value):
-            format_value_c9 = radix_short.format_c9(value)
-            format_value_c10 = radix_short.format_c10(value)
-            set_values(data_frame, index, format_value_c9, format_value_c10)
-        elif FormatUtil.is_long(value):
-            format_value_c9 = radix_long.format_c9(value)
-            format_value_c10 = radix_long.format_c10(value)
-            set_values(data_frame, index, format_value_c9, format_value_c10)
-        else:
-            set_values(data_frame, index, '0', '0')
+    data_frame['RADICADO_C10'] = data_frame['RADICACION'].apply(lambda x: (
+        radix_short.format_c10(virgin_value(x, False)) if format_util.is_short(virgin_value(x, False))
+        else radix_long.format_c10(virgin_value(x, False))
+    ))
 
 
 def set_values(data_frame, index, value_c9, value_c10):
-    data_frame.at[index, 'RADICADO_C9'] = value_c9
-    data_frame.at[index, 'RADICADO_C10'] = value_c10
+    data_frame.loc[index, 'RADICADO_C9'] = str(value_c9)
+    data_frame.loc[index, 'RADICADO_C10'] = value_c10
 
-    if value_c9 is None:
-        data_frame.at[index, 'LARGO_RAD_C9'] = 0
-    else:
-        data_frame.at[index, 'LARGO_RAD_C9'] = str(len(value_c9))
 
-    if value_c10 is None:
-        data_frame.at[index, 'LARGO_RAD_C10'] = 0
-    else:
-        data_frame.at[index, 'LARGO_RAD_C10'] = str(value_c9).__len__()
+def virgin_value(value, with_dot=True):
+    from radix_organizer.util import format_util
+    clean_value = format_util.clean_numbers(value)
+    return str("." + clean_value if with_dot else clean_value)
